19782231a732915594fd7e0bc9ff43dee8f4015b
grh-gitee <3494242908@qq.com>
2023-01-03 21:56:48 +0800

    feat: 添加中值方法

--- src/statistics/__test__/median.ts
+++ src/statistics/__test__/median.ts
@@ -6,8 +6,8 @@ UnitTest(median)
 		{ param: [], tobe: null },
 		{ param: [1], tobe: 1 },
 		{ param: [1, 2], tobe: 1.5 },
-		{ param: [1, 2, 3, 0], tobe: null },
-		{ param: [1, 2, 3, 3, 3, 3, 2, 1, 0], tobe: 3 },
+		{ param: [1, 2, 3, 0], tobe: 1.5 },
+		{ param: [1, 2, 3, 3, 3, 3, 2, 1, 0], tobe: 2 },
 		{ param: [1, 2, 3, 3, 3, 3, 12, 12, 12], tobe: 3 },
 	)
 	.run()
