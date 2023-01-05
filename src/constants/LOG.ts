4e8380c5b430ac66ec9ed254983f08b4cca76793
grh-gitee <3494242908@qq.com>
2023-01-05 21:15:41 +0800

    feat: 添加常用变量

---
+++ src/constants/LOG.ts
@@ -0,0 +1,16 @@
+/**
+ * 2 的自然对数，约等于 0.693。
+ */
+export const LN2 = Math.LN2
+/**
+ * 10 的自然对数，约等于 2.303。
+ */
+export const LN10 = Math.LN10
+/**
+ * 以 2 为底的 E 的对数，约等于 1.443。
+ */
+export const LOG2E = Math.LOG2E
+/**
+ * 以 10 为底的 E 的对数，约等于 0.434。
+ */
+export const LOG10E = Math.LOG10E
---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+/**
+ * 均保留到小数点后16位
+ */
+export * from './BASE'
+export * from './LOG'
+export * from './SQRT'
